d45fd18f41f582d0f42366c5aac81651fe43bf94
tradingbysanzid-design <tradingbysanzid@gmail.com>
2026-05-30 00:31:26 +0600

    Tailwind CSS configuration

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#a855f7',
+        'primary-dark': '#7e22ce',
+      },
+    },
+  },
+  plugins: [],
+}
